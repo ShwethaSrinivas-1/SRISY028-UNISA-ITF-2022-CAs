18f7b33b34e9974cbff6fb031a3e623dd7c579cb
Shwetha Srinivas <srisy028@mymail.unisa.edu.au>
2022-02-20 19:14:47 +1100

    add final file ca4

--- CA4/app.js
+++ CA4/app.js
@@ -11,30 +11,41 @@ rootEl.setAttribute("data-theme", getTheme);
 const headerEl = document.getElementById("mainHeading");
 
 function headingChange() {
+	//////////get the query sting value from the url, if it exists////////////////
 	const queryString = window.location.search;
 	const stringFromUrl = new URLSearchParams(queryString);
 	const userName = stringFromUrl.get("user_name");
 
+	//////////// get the value of 'name' key from localStorage, if it exists//////////
 	let getName = localStorage.getItem("name");
 
+	////////////update the heading element with the value from the query string//////////////
 	if (queryString.includes("user_name")) {
 		headerEl.textContent = `Hello, ${userName}`;
+		///////////assign that value to the'name' key in localStorage/////////////
 		localStorage.setItem("name", userName);
+
+		////////////update the heading element with the value from localStoragethe localStorage////////////
 	} else if (!queryString.includes("user_name") && getName !== null) {
 		headerEl.textContent = `Hello, ${getName}`;
+		/////////// if both local storage and query string are empty, update the heading element with 'header'//////////
 	} else {
 		headerEl.textContent = "Header";
 	}
 }
 
 function modeSwitch() {
+	///////get the theme from localStorage,if it exists//////////
 	let newColors = rootEl.getAttribute("data-theme");
 
+	/////////if new colors is dark, update the root element to light////////
 	if (newColors === "dark") {
 		rootEl.setAttribute("data-theme", "light");
+		////////if new colors is light, update the root element to dark////////
 	} else {
 		rootEl.setAttribute("data-theme", "dark");
 	}
 	newColors = rootEl.getAttribute("data-theme");
+	/////store the user preffered color to local storage/////////////
 	localStorage.setItem("data-theme", newColors);
 }
